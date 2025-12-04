44e3914575078d513eee14109a781ef49a335083
novanjanis322 <novangfc@gmail.com>
2025-12-04 13:19:30 +0700

    fix column name and file naming consistency

--- medium/1193_monthly_transactions_i.sql
+++ medium/1193_monthly_transactions_i.sql
@@ -28,14 +28,18 @@
 SELECT
     DATE_FORMAT(trans_date, '%Y-%m') as month,
     country,
-    COUNT(CASE WHEN state = 'approved' THEN 1 END) as trans_count,
-    COUNT(state) as approved_count,
+    COUNT(state) as trans_count,
+    COUNT(CASE WHEN state = 'approved' THEN 1 END) as approved_count,
     SUM(amount) as trans_total_amount,
     SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) as approved_total_amount
 FROM 
     Transactions
 GROUP BY 
     DATE_FORMAT(trans_date, "%Y-%m"), country
+
+
+
+
 
 
 
