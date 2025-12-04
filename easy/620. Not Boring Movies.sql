26ec2dbf6ec33ad4cf22e37734fd44fbb9c86e50
novanjanis322 <novangfc@gmail.com>
2025-12-04 13:03:01 +0700

    Day 3: SQL basics - 1 easy 1 medium queries (Aggregation)

---
+++ easy/620. Not Boring Movies.sql
@@ -0,0 +1,36 @@
+-- SQL Schema
+-- Pandas Schema
+-- Table: Cinema
+
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | id             | int      |
+-- | movie          | varchar  |
+-- | description    | varchar  |
+-- | rating         | float    |
+-- +----------------+----------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row contains information about the name of a movie, its genre, and its rating.
+-- rating is a 2 decimal places float in the range [0, 10]
+ 
+
+-- Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
+
+-- Return the result table ordered by rating in descending order.
+
+--Link: https://leetcode.com/problems/not-boring-movies/
+
+# Write your MySQL query statement below
+SELECT 
+    id, movie, description, rating
+FROM
+    Cinema
+WHERE
+    id % 2 = 1
+AND
+    description != "boring"
+ORDER BY
+    rating
+DESC
+;
---
+++ medium/1193. Monthly Transactions I.sql
@@ -0,0 +1,41 @@
+-- SQL Schema
+-- Pandas Schema
+-- Table: Transactions
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | country       | varchar |
+-- | state         | enum    |
+-- | amount        | int     |
+-- | trans_date    | date    |
+-- +---------------+---------+
+-- id is the primary key of this table.
+-- The table has information about incoming transactions.
+-- The state column is an enum of type ["approved", "declined"].
+ 
+
+-- Write an SQL query to find for each month and country, the number of transactions and their total amount, the number of approved transactions and their total amount.
+
+-- Return the result table in any order.
+
+-- The query result format is in the following example.
+
+-- Link: https://leetcode.com/problems/monthly-transactions-i/ 
+
+# Write your MySQL query statement below
+SELECT
+    DATE_FORMAT(trans_date, '%Y-%m') as month,
+    country,
+    COUNT(CASE WHEN state = 'approved' THEN 1 END) as trans_count,
+    COUNT(state) as approved_count,
+    SUM(amount) as trans_total_amount,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) as approved_total_amount
+FROM 
+    Transactions
+GROUP BY 
+    DATE_FORMAT(trans_date, "%Y-%m"), country
+
+
+
