fbaf63b57e62e0ee3d65bd89793e1a0c4241f96b
novanjanis322 <novangfc@gmail.com>
2025-12-23 18:08:42 +0700

    Week 2 Day 1: SQL Advanced (5/15): - Window functions: 4 medium + 1 hard (RANK, DENSE_RANK, PARTITION BY)

---
+++ medium/177_nth_highest_salary.sql
@@ -0,0 +1,77 @@
+-- Medium
+-- Topics
+-- premium lock icon
+-- Companies
+-- SQL Schema
+-- Pandas Schema
+-- Table: Employee
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | id          | int  |
+-- | salary      | int  |
+-- +-------------+------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table contains information about the salary of an employee.
+ 
+
+-- Write a solution to find the nth highest distinct salary from the Employee table. If there are less than n distinct salaries, return null.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Employee table:
+-- +----+--------+
+-- | id | salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+-- n = 2
+-- Output: 
+-- +------------------------+
+-- | getNthHighestSalary(2) |
+-- +------------------------+
+-- | 200                    |
+-- +------------------------+
+-- Example 2:
+
+-- Input: 
+-- Employee table:
+-- +----+--------+
+-- | id | salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- +----+--------+
+-- n = 2
+-- Output: 
+-- +------------------------+
+-- | getNthHighestSalary(2) |
+-- +------------------------+
+-- | null                   |
+-- +------------------------+
+
+-- Link: https://leetcode.com/problems/nth-highest-salary/description/
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+    SELECT
+        DISTINCT salary
+    FROM
+        (
+            SELECT
+                salary,
+                DENSE_RANK() OVER (ORDER BY salary DESC) as salary_rank
+            FROM
+                Employee
+        ) as e
+    WHERE salary_rank = N
+  );
+END
